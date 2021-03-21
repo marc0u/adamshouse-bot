04a5094fc9010498fdba09b7995ce4426b92b32d
Marco Urriola <marco.urriola@gmail.com>
2021-03-21 19:26:04 -0300

    Added dotenv feature

--- bot_AdamsHouse.py
+++ bot_AdamsHouse.py
@@ -1,8 +1,15 @@
-import logging
-from os import getenv
-from threading import Thread
-from src.botloop import Bot
-from src.netloop import NetControl
+# Load vatiablers from .env file
+from dotenv import load_dotenv, find_dotenv
+try:
+    load_dotenv(find_dotenv(raise_error_if_not_found=True), override=True)
+except:
+    raise
+else:
+    import logging
+    from os import getenv
+    from threading import Thread
+    from src.botloop import Bot
+    from src.netloop import NetControl
 
 # Logging
 filename = getenv("HOME") + "/logs/" + "AdamsHouse" + ".log"
