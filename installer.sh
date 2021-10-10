27878e154594e48b2c67f08212d35aa1ea87c811
Marco Urriola <marco.urriola@gmail.com>
2021-10-10 18:44:31 -0300

    installer fixed

--- installer.sh
+++ installer.sh
@@ -1,7 +1,6 @@
-PY_APPNAME="adamshousebot"
 echo "Removing previous version..."
-docker stop $PY_APPNAME
-docker container rm $PY_APPNAME 
+docker stop adamshousebot
+docker container rm adamshousebot 
 echo "Creating new container..."
-docker run -dit --name=$PY_APPNAME -v $PWD:/app -e TZ=America/Santiago -e APPNAME="${PY_APPNAME}.py" --restart=unless-stopped --memory=512m --cpus="0.5" marc0u/python-launcher
+docker run -dit --name=adamshousebot -v $PWD:/app -e TZ=America/Santiago -e APPNAME=adamshousebot.py --restart=unless-stopped --memory=512m --cpus="0.5" marc0u/python-launcher
 echo "Done!"
